6e9e458ae6d4d6178361a33c557b4248be52bbc9
jkrol1 <jk.jankrol@gmail.com>
2021-01-02 20:13:34 +0100

    Initial commit

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+
+class DevelopmentConfig(Config):
+    pass
+
+
+class TestingConfig(Config):
+    pass
+
+
+class ProductionConfig(Config):
+    pass
+
+
+config = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
+    "default": DevelopmentConfig,
+}
